6d8ee5858b5effadc9c0c74108ade7a7de415d00
Peter Neamtu <neamtupeter@gmail.com>
2023-11-24 23:59:16 +0200

    Testing button functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,15 +11,16 @@ console.log(cardDeck);
 
 const mainComponent = document.querySelector('#cards-container');
 
-let card1 = cardDeck.lastCard;
-console.log(card1);
-let drawnCardComponent1 = new DrawnCardComponent(card1, ['card-left']);
-mainComponent.append(drawnCardComponent1.cardContainerElement);
+function newRound() {
+    let card1 = cardDeck.lastCard;
+    console.log(card1);
+    let drawnCardComponent1 = new DrawnCardComponent(card1, ['card-left']);
+    mainComponent.append(drawnCardComponent1.cardContainerElement);
 
-let card2 = cardDeck.lastCard;
-console.log(card2);
-let drawnCardComponent2 = new DrawnCardComponent(card2, ['card-right']);
-mainComponent.append(drawnCardComponent2.cardContainerElement);
-
+    let card2 = cardDeck.lastCard;
+    console.log(card2);
+    let drawnCardComponent2 = new DrawnCardComponent(card2, ['card-right']);
+    mainComponent.append(drawnCardComponent2.cardContainerElement);
+}
 // console.log(structuredClone(cardDeck));
 // console.log(cardDeck.cardsSet);
