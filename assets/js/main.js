0ddfe63cc7dc0260b1ddb8a2dce4709c60e99943
Peter Neamtu <neamtupeter@gmail.com>
2023-11-25 18:37:07 +0200

    Setting players score and game winner.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ import cards from './cards.js';
 import CardDeck from './card-deck.js';
 import DrawnCardComponent from './drawn-card-component.js';
 
-const cardDeckSize = 10;
+const cardDeckSize = 20;
 const cardDeck = new CardDeck(cards, cardDeckSize);
 
 console.log(cardDeck);
@@ -12,23 +12,39 @@ console.log(cardDeck);
 // const mainComponent = document.querySelector('#cards-container');
 const player1 = document.querySelector('.player1');
 const player2 = document.querySelector('.player2');
+const score1 = document.querySelector('.score1');
+const score2 = document.querySelector('.score2');
+const winner = document.querySelector('.winner');
 
 const button = document.querySelector('.game-btn');
 button.addEventListener('click', newRound);
 
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
 function newRound() {
-    emptyCard(player1);
-    emptyCard(player2);
+    console.log(cardDeck.cardsSet.length);
+    if (cardDeck.cardsSet.length > 0) {
+        emptyCard(player1);
+        emptyCard(player2);
 
-    let card1 = cardDeck.lastCard;
-    console.log(card1);
-    let drawnCardComponent1 = new DrawnCardComponent(card1, ['card-left']);
-    player1.append(drawnCardComponent1.cardContainerElement);
+        let card1 = cardDeck.lastCard;
+        console.log(card1);
+        let drawnCardComponent1 = new DrawnCardComponent(card1, ['card-left']);
+        player1.append(drawnCardComponent1.cardContainerElement);
+        scorePlayer1 += card1.points;
+        score1.textContent = scorePlayer1;
 
-    let card2 = cardDeck.lastCard;
-    console.log(card2);
-    let drawnCardComponent2 = new DrawnCardComponent(card2, ['card-right']);
-    player2.append(drawnCardComponent2.cardContainerElement);
+        let card2 = cardDeck.lastCard;
+        console.log(card2);
+        let drawnCardComponent2 = new DrawnCardComponent(card2, ['card-right']);
+        player2.append(drawnCardComponent2.cardContainerElement);
+        scorePlayer2 += card2.points;
+        score2.textContent = scorePlayer2;
+    } else {
+        winner.textContent =
+            scorePlayer1 > scorePlayer2 ? 'Player1' : 'Player2';
+    }
 }
 
 function emptyCard(parentElem) {
