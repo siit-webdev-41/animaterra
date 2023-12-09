e1448731a0bbcb3243fcb1a1a70245a5c90dc3c4
cristiursulescu <ursulescucristian87@outlook.com>
2023-12-10 01:14:13 +0200

    create Class Player

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,45 @@
-console.log('main.js is loaded....');
+console.log("main.js is loaded....");
 
-import cards from './cards.js';
-import CardDeck from './card-deck.js';
-import DrawnCardComponent from './drawn-card-component.js';
+import cards from "./cards.js";
+import CardDeck from "./card-deck.js";
+import Player from "./player.js";
 
 const cardDeckSize = 20;
 const cardDeck = new CardDeck(cards, cardDeckSize);
 
 console.log(cardDeck);
 
-// const mainComponent = document.querySelector('#cards-container');
+const mainComponent = document.querySelector("#cards-container");
 
 // player 1
-const score1 = document.querySelector('.score1');
-const player1 = document.querySelector('.player1');
-const drawnCardComponent1 = new DrawnCardComponent(null, ['card-left']);
-player1.append(drawnCardComponent1.cardContainerElement);
-let scorePlayer1 = 0;
+const score1 = document.querySelector(".score1");
+const player1 = document.querySelector(".player1");
+const playerUser1 = new Player(score1, player1, "card-left");
 
 //  player 2
-const score2 = document.querySelector('.score2');
-const player2 = document.querySelector('.player2');
-const drawnCardComponent2 = new DrawnCardComponent(null, ['card-right']);
-player2.append(drawnCardComponent2.cardContainerElement);
-let scorePlayer2 = 0;
+const score2 = document.querySelector(".score2");
+const player2 = document.querySelector(".player2");
+const playerUser2 = new Player(score2, player2, "card-right");
 
-const winner = document.querySelector('.winner');
+const winner = document.querySelector(".winner");
 
-const button = document.querySelector('.game-btn');
-button.addEventListener('click', newRound);
-
+const button = document.querySelector(".game-btn");
+button.addEventListener("click", newRound);
 
 function newRound() {
     console.log(cardDeck.cardsSet.length);
     if (cardDeck.cardsSet.length > 0) {
         let card1 = cardDeck.lastCard;
         console.log(card1);
-        drawnCardComponent1.uppdateComponent(card1);
-
-        scorePlayer1 += card1.points;
-        score1.textContent = scorePlayer1;
+        playerUser1.updateDrawnCard(card1);
+        playerUser1.updateScore(card1);
 
         let card2 = cardDeck.lastCard;
         console.log(card2);
-        drawnCardComponent2.uppdateComponent(card2);
-        
-        scorePlayer2 += card2.points;
-        score2.textContent = scorePlayer2;
+        playerUser2.updateDrawnCard(card2);
+        playerUser2.updateScore(card2);
     } else {
         winner.textContent =
-            scorePlayer1 > scorePlayer2 ? 'Player1' : 'Player2';
+            playerUser1.score > playerUser2.score ? "Player1" : "Player2";
     }
 }
-
-// console.log(structuredClone(cardDeck));
-// console.log(cardDeck.cardsSet);
