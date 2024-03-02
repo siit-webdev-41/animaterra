2c71475e8538647f02d489a049e7b7134032a707
cristiursulescu <ursulescucristian87@outlook.com>
2024-03-03 00:07:35 +0200

    add rounds and endGame in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
-console.log('main.js is loaded....');
+console.log("main.js is loaded....");
 
-import cards from './cards.js';
-import CardDeck from './card-deck.js';
-import PlayerComponent from './player-component.js';
-import PlayerModel from './player.js';
-import DrawnCardComponent from './drawn-card-component.js';
+import cards from "./cards.js";
+import CardDeck from "./card-deck.js";
+import PlayerComponent from "./player-component.js";
+import PlayerModel from "./player.js";
+import DrawnCardComponent from "./drawn-card-component.js";
 
 const cardDeckSize = 20;
 const cardDeck = new CardDeck(cards, cardDeckSize);
 
 console.log(cardDeck);
+
+const winnerPlayer = document.querySelector(".winner");
 
 const playersComponent = document.querySelector("#players-ui-component");
 const playerCardsComponent = document.querySelector("#cards-container");
@@ -17,7 +19,12 @@ const playerCardsComponent = document.querySelector("#cards-container");
 const button = document.querySelector(".game-btn");
 button.addEventListener("click", newRound);
 
+let roundNr = 0;
+const roundsMax = 20;
+
 function newRound() {
+    roundNr++;
+
     for (let i = 0; i < players.length; i++) {
         const drawnCard = cardDeck.lastCard;
         console.log(drawnCard);
@@ -32,8 +39,32 @@ function newRound() {
         // update player component
         playerComponents[i].update();
     }
+
+    if (roundNr >= roundsMax) {
+        button.removeEventListener("click", newRound);
+        endGame();
+    } else {
+        const remainingPlayers = players.filter((player) => player.score > 0);
+        if (remainingPlayers.length <= 1) {
+            button.removeEventListener("click", newRound);
+            endGame();
+        }
+    }
 }
 
+function endGame() {
+    let highestScore = -1;
+    let winner;
+
+    players.forEach((player) => {
+        if (player.score > highestScore) {
+            highestScore = player.score;
+            winner = player;
+        }
+    });
+
+    winnerPlayer.innerHTML = `The winner is ${winner.name} with ${highestScore} points.`;
+}
 
 const startScore = 1;
 const players = [
@@ -60,12 +91,12 @@ players.forEach((player) => {
     playerCardsComponent.append(drawnCardComponent.cardContainerElement);
 });
 
-const backOfCardDeck = document.querySelector('.card-stack');
+const backOfCardDeck = document.querySelector(".card-stack");
 // console.log(backOfCard);
 
 for (let i = 0; i < 3; i++) {
-    const backOfCard = document.createElement('img');
-    backOfCard.src = 'assets/imgs/back-of-card.jpg';
+    const backOfCard = document.createElement("img");
+    backOfCard.src = "assets/imgs/back-of-card.jpg";
     backOfCard.classList.add(`back-of-card-${i}`);
     backOfCardDeck.append(backOfCard);
 }
